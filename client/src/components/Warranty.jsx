0e7b86dfa9bf66771bff4d514bf34429f21d911e
virendraPrajapat18 <166205346+virendraPrajapat18@users.noreply.github.com>
2025-09-23 10:17:06 +0530

    Update Warranty.jsx

--- client/src/components/Warranty.jsx
+++ client/src/components/Warranty.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Warranty = () => {
   return (
-    <div className='w-full flex justify-center h-[270px]'>
-      <img src="warranty.png" alt="" />
+    <div className="w-full flex justify-center items-center py-6 px-4">
+      <img
+        src="warranty.png"
+        alt="Warranty"
+        className="w-full max-w-[1200px] h-auto object-contain"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Warranty
+export default Warranty;
